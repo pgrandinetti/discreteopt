dec87de2dc33135c075feee530c4d9cd03b0f051
pietro <pietro.gra@hotmail.it>
2017-11-24 16:22:33 +0100

    Coloring 41/60

--- coloring/solver.py
+++ coloring/solver.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 from scipy.sparse import lil_matrix
 import pdb
-global EDGES, DEG
+import random
+random.seed(1847859218408232171737)
+global EDGES, DEG, DEPTH
 
 def solve_it(input_data):
-    global EDGES, DEG
+    global EDGES, DEG, DEPTH
     # Modify this code to run your optimization algorithm
 
     # parse the input
@@ -28,12 +30,17 @@ def solve_it(input_data):
     DEG = make_degree()
     sol = [-1] * EDGES.shape[0]
 
+    # set max depth of search expansion
+    DEPTH = EDGES.shape[0]
+
     # build a trivial solution
     # every node has its own color
     #solution = range(0, node_count)
 
-    solution = search().sol
-    obj = max(solution) + 1
+    exp_fnc = 'best_node'
+
+    solution = search(exp_fnc=exp_fnc).sol
+    obj = len(set(solution))
 
     # prepare the solution in the specified output format
     output_data = str(obj) + ' ' + str(0) + '\n'
@@ -41,10 +48,11 @@ def solve_it(input_data):
 
     return output_data
 
-def search():
+def search(exp_fnc='best_node'):
     global EDGES
     best = None
     best_deg = best_node([-1]*EDGES.shape[0])
+    #best_deg = random.randint(0, EDGES.shape[0]-1)
     first_sol = [-1]*EDGES.shape[0]
     first_sol[best_deg] = 0
     curr = CPNode(first_sol)
@@ -56,27 +64,44 @@ def search():
                 node.value < best.value:
                 best = node
         else:
-            novels = node.expand()
+            novels = node.expand(exp_fnc)
             for new in novels[::-1]:
                 if best is None or\
                     new.value < best.value:
                     to_expand.append(new)
-    assert(assert_sol(best))
+    #assert(assert_sol(best))
     return best
 
 def make_degree():
     global EDGES
     deg = [0]*EDGES.shape[0]
     for i in range(EDGES.shape[0]):
-        deg[i] = EDGES[i,:].nnz + EDGES[:,i].nnz
+        deg[i] = (i, EDGES[i,:].nnz + EDGES[:,i].nnz)
+    deg.sort(key=lambda x: x[1], reverse=True)
     return deg
 
 def best_node(curr_sol):
     global DEG
-    degrees = sorted(DEG, reverse=True)
-    for i in range(len(degrees)):
-        if curr_sol[degrees[i]] == -1:
-            return DEG.index(degrees[i])
+    for i in DEG:
+        if curr_sol[i[0]] == -1:
+            return i[0]
+    return None
+
+def max_constr(curr_sol):
+    constr = [(i, 0) for i in range(len(curr_sol))]
+    for i in range(len(curr_sol)):
+        rows, cols = EDGES[i,:].nonzero()
+        for c in cols:
+            if curr_sol[c] != -1:
+                constr[i] = (constr[i][0], constr[i][1] + 1)
+        rows, cols = EDGES[:,i].nonzero()
+        for r in rows:
+            if curr_sol[r] != -1:
+                constr[i] = (constr[i][0], constr[i][1] + 1)
+    constr.sort(key=lambda x: x[1], reverse=True)
+    for i in constr:
+        if curr_sol[i[0]] == -1:
+            return i[0]
     return None
 
 def has_edge(i, j):
@@ -88,58 +113,63 @@ class CPNode():
     def __init__(self, solution):
         global EDGES
         self.sol = solution
-        self.next_child = 0
         self.feasible = True
+        """
         rows, cols = EDGES.nonzero()
         for r,c in zip(rows, cols):
             if self.sol[r] != -1 and\
                 self.sol[r] == self.sol[c]:
                 self.feasible = False
                 break
+        """
         if self.feasible:
             self.is_leaf = (sum(x == -1 for x in self.sol) == 0)
             self.value = max(self.sol)
 
-    def can_expand(self):
-        return self.next_child < sum(x == -1 for x in self.sol)
-
-    def expand(self):
-        all_sol = self.list_to_assign()
-        novel = all_sol[self.next_child]
-        self.next_child += len(self.sol)+1
+    def expand(self, exp_fnc):
+        fnc = globals()[exp_fnc]
+        if self.is_leaf:
+            return []
+        novel = fnc(self.sol)
         available = assign_value(self.sol, novel)
         to_ret = []
-        for c in sorted(available):
+        for c in available:
             new_sol = self.sol.copy()
             new_sol[novel] = c
             to_ret.append(CPNode(new_sol))
         return to_ret
 
-    def list_to_assign(self):
-        global DEG
-        to_assign = []
-        for idx, c in enumerate(self.sol):
-            if c == -1:
-                to_assign.append(idx)
-        to_assign.sort(key=lambda x: DEG[x], reverse=True)
-        return to_assign
 
 def assign_value(curr_sol, idx):
-    global EDGES
+    global EDGES, DEPTH
     new_sol = curr_sol.copy()
     all_values = set()
-    rows, cols = EDGES.nonzero()
-    for r,c in zip(rows, cols):
-        if r == idx:
-            all_values.add(curr_sol[c])
-        elif c == idx:
-            all_values.add(curr_sol[r])
-    available = set()
-    for i in range(max(curr_sol) + 1):
-        if not i in all_values:
-            available.add(i)
-    if not available: available.add(max(all_values) + 1)
-    return [min(available)]
+    rows, cols = EDGES[idx,:].nonzero()
+    for c in cols:
+        all_values.add(curr_sol[c])
+    rows, cols = EDGES[:,idx].nonzero()
+    for r in rows:
+        all_values.add(curr_sol[r])
+    if not all_values:
+        return 0
+    available = set(range(max(curr_sol) + 1))
+    available = available.difference(all_values)
+    if not available:
+        return [max(all_values) + 1]
+    if len(available) == 1:
+        return available
+    available = list(sorted(available))
+    # decide behavior according to problem size
+    if EDGES.shape[0] <= 70:
+        return available[:DEPTH]
+    elif EDGES.shape[0] == 250:
+        best = available[0]
+        for a in available:
+            if curr_sol.count(best) < curr_sol.count(a):
+                best = a
+        return [best]
+    else:
+        return [available[0]]
 
 def assert_sol(best):
     global EDGES
