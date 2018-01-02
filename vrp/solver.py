56a9f958e769a409e076bb54a393f70be1918212
pietro <pietro.gra@hotmail.it>
2018-01-02 13:38:56 +0100

    VRP 34/60

--- vrp/solver.py
+++ vrp/solver.py
@@ -3,6 +3,13 @@
 
 import math
 from collections import namedtuple
+import random
+import time
+from datetime import datetime
+from copy import deepcopy
+import pdb
+
+global DEPOT
 
 Customer = namedtuple("Customer", ['index', 'demand', 'x', 'y'])
 
@@ -10,6 +17,8 @@ def length(customer1, customer2):
     return math.sqrt((customer1.x - customer2.x)**2 + (customer1.y - customer2.y)**2)
 
 def solve_it(input_data):
+    global DEPOT
+    random.seed(1659163401)
     # Modify this code to run your optimization algorithm
 
     # parse the input
@@ -28,17 +37,21 @@ def solve_it(input_data):
 
     #the depot is always the first customer in the input
     depot = customers[0] 
-
+    DEPOT = depot
 
     # build a trivial solution
     # assign customers to vehicles starting by the largest customer demands
     vehicle_tours = trivial_sol(customers, depot, vehicle_count, vehicle_capacity)
 
+    # use local search
+    limit = time_map(customer_count, vehicle_count)
+    vehicle_tours = local_search(customers, vehicle_tours, vehicle_capacity, time_limit=limit)
+
     # checks that the number of customers served is correct
     assert sum([len(v) for v in vehicle_tours]) == len(customers) - 1
 
     # calculate the cost of the solution; for each vehicle the length of the route
-    obj = state_value(vehicle_tours, depot)
+    obj = state_value(vehicle_tours)
 
     # prepare the solution in the specified output format
     outputData = '%.2f' % obj + ' ' + str(0) + '\n'
@@ -46,6 +59,18 @@ def solve_it(input_data):
         outputData += str(depot.index) + ' ' + ' '.join([str(customer.index) for customer in vehicle_tours[v]]) + ' ' + str(depot.index) + '\n'
 
     return outputData
+
+def time_map(customer_count, vehicle_count):
+    # give different time limits to problem instances
+    # times are in seconds
+    if customer_count < 50:
+        return 60
+    elif customer_count < 90:
+        return 300 # instance 3
+    elif customer_count < 400:
+        return 300
+    else:
+        return 300 # instance 6
 
 def trivial_sol(customers, depot, vehicle_count, vehicle_capacity):
     vehicle_tours = []
@@ -69,16 +94,72 @@ def trivial_sol(customers, depot, vehicle_count, vehicle_capacity):
             remaining_customers -= used
     return vehicle_tours
 
-def state_value(veh_tours, depot):
+def state_value(veh_tours):
+    global DEPOT
     obj = 0
     for v in range(len(veh_tours)):
         vehicle_tour = veh_tours[v]
         if len(vehicle_tour) > 0:
-            obj += length(depot, vehicle_tour[0])
+            obj += length(DEPOT, vehicle_tour[0])
             for i in range(0, len(vehicle_tour)-1):
                 obj += length(vehicle_tour[i], vehicle_tour[i+1])
-            obj += length(vehicle_tour[-1], depot)    
+            obj += length(vehicle_tour[-1], DEPOT)    
     return obj
+
+def accept(current, novel, temperature):
+    old_val = state_value(current)
+    new_val = state_value(novel)
+    if new_val <= old_val:
+        return True
+    if math.exp(-abs(new_val - old_val) / temperature) > random.random():
+        return True
+    return False
+
+def find_neigh(curr_sol, customers, vehicle_capacity):
+    neigh = deepcopy(curr_sol)
+    r1 = random.randint(1, len(customers)-1) # what customer to move...
+    d = customers[r1].demand
+    available = []
+    for veh_tour in neigh:
+        if customers[r1] in veh_tour:
+            veh_tour.remove(customers[r1])
+        tot = vehicle_capacity
+        for cus in veh_tour:
+            tot -= cus.demand
+        available.append(tot)
+    s = [(i,x) for (i,x) in enumerate(available) if x>=d]
+    r2 = s[random.randint(0, len(s)-1)][0] # ... to what vehicle...
+    r3 = random.randint(0, len(curr_sol[r2])) # ... in what position
+    neigh[r2].insert(r3, customers[r1])
+    return neigh
+
+def local_search(customers, guess, vehicle_capacity, time_limit=120):
+    best = deepcopy(guess)
+    current = guess
+    restart = 0
+    counter = 0
+    T = len(customers)
+    alpha = 0.999
+    min_T = 1e-8
+    start = time.time()
+    diff = time.time() - start
+    print('Local search starts at {}'.format(datetime.now().time()))
+    while diff < time_limit:
+        if T <= min_T:
+            T = len(customers)
+            restart += 1
+        neigh = find_neigh(current, customers, vehicle_capacity)
+        if neigh is not None:
+            if accept(current, neigh, T):
+                current = neigh
+                if state_value(current) < state_value(best):
+                    best = deepcopy(current)
+        counter += 1
+        T *= alpha
+        diff = time.time() - start
+    print('Returning solution after {} iteration and {} restarts at {}'.format(counter, restart, datetime.now().time()))
+    return best
+
 
 import sys
 
